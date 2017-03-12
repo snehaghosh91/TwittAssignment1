10967430f8ae98d97713efcc0dd727d11577c8a8
snehaghosh91 <sg3533@nyu.edu>
2017-03-11 23:40:26 -0500

    deployingapp

--- Assignment1/wsgi.py
+++ Assignment1/wsgi.py
@@ -9,8 +9,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+os.environ("DJANGO_SETTINGS_MODULE", "Assignment1.settings")
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Assignment1.settings")
-
 application = get_wsgi_application()
