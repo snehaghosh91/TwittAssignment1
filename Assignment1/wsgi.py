f25d3df2b2e982d676ce1c559d41065aab4166a7
snehaghosh91 <sg3533@nyu.edu>
2017-03-11 23:56:39 -0500

    deployingapp

--- Assignment1/wsgi.py
+++ Assignment1/wsgi.py
@@ -7,9 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys, site
+
+
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+WORK_DIRECTORY = os.path.join(CURRENT_DIRECTORY, '..')
+site.addsitedir('/usr/local/lib/python2.7/site-packages')
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "Assignment1.settings"
 
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
