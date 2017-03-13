fd8d1860b12be39500aab505c96872f4ba0d6080
snehaghosh91 <sg3533@nyu.edu>
2017-03-12 20:58:29 -0400

    config

--- Assignment1/wsgi.py
+++ Assignment1/wsgi.py
@@ -7,13 +7,31 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os, sys, site
+import os
+import site
+import sys
 
+# Remember original sys.path.
+prev_sys_path = list(sys.path) 
 
+# we add currently directory to path and change to it
+pwd = os.path.dirname(os.path.abspath(__file__))
+os.chdir(pwd)
+sys.path = [pwd] + sys.path
 
-CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-WORK_DIRECTORY = os.path.join(CURRENT_DIRECTORY, '..')
-site.addsitedir('/opt/python/run/venv/lib/python2.7/site-packages')
+# find the site-packages within the local virtualenv
+for python_dir in os.listdir('lib'):
+    site_packages_dir = os.path.join('lib', python_dir, 'site-packages')
+    if os.path.exists(site_packages_dir):
+        site.addsitedir(os.path.abspath(site_packages_dir))
+
+# Reorder sys.path so new directories at the front.
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "Assignment1.settings"
 
